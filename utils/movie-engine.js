988499e0269d79aff8fb68b604d9de68a4c2ad39
Strateva <MiroslavaStrateva@gmail.com>
2016-11-21 23:50:34 +0200

    Add promisses in movie-engine

--- utils/movie-engine.js
+++ utils/movie-engine.js
@@ -14,50 +14,62 @@ require("../config/mongoose")(constants.connectionString);
 
 module.exports = {
     generateSimpleMovies() {
-        let urlsQueue = queuesFactory.getQueue();
-        const simpleMovieUrlTemplate = _.template("http://www.imdb.com/search/title?genres=<%=genre%>&title_type=feature&0sort=moviemeter,asc&page=<%=i+1%>&view=simple&ref_=adv_nxt");
+        let promise = new Promise((resolve) => {
+            let urlsQueue = queuesFactory.getQueue();
+            const simpleMovieUrlTemplate = _.template("http://www.imdb.com/search/title?genres=<%=genre%>&title_type=feature&0sort=moviemeter,asc&page=<%=i+1%>&view=simple&ref_=adv_nxt");
 
-        constants.genres.forEach(genre => {
-            for (let i = 0; i < constants.pagesCount; i += 1) {
-                let url = simpleMovieUrlTemplate({ genre, i });
-                urlsQueue.push(url);
-            }
+            constants.genres.forEach(genre => {
+                for (let i = 0; i < constants.pagesCount; i += 1) {
+                    let url = simpleMovieUrlTemplate({ genre, i });
+                    urlsQueue.push(url);
+                }
+            });
+
+            Array.from({ length: constants.asyncPagesCount })
+                .forEach(() => simpleMovieScrapper.getMoviesFromUrls(urlsQueue));
+            resolve();
         });
-
-        Array.from({ length: constants.asyncPagesCount })
-            .forEach(() => simpleMovieScrapper.getMoviesFromUrls(urlsQueue));
+        return promise;
     },
     generateMovies() {
-        let moviesQueue = queuesFactory.getQueue();
-        let movieUrlTemplate = _.template("http://www.imdb.com/title/<%=movieId%>");
+        let promise = new Promise((resolve) => {
+            let moviesQueue = queuesFactory.getQueue();
+            let movieUrlTemplate = _.template("http://www.imdb.com/title/<%=movieId%>");
 
-        modelsFactory.showSimpleMovies()
-            .then((movies) => {
-                for (let movie of movies) {
-                    let movieId = movie.imdbId;
-                    let url = movieUrlTemplate({ movieId });
-                    moviesQueue.push(url);
-                }
-            })
-            .then(() => {
-                movieScrapper.getMoviesInfoFromUrls(moviesQueue);
-            });
+            modelsFactory.showSimpleMovies()
+                .then((movies) => {
+                    for (let movie of movies) {
+                        let movieId = movie.imdbId;
+                        let url = movieUrlTemplate({ movieId });
+                        moviesQueue.push(url);
+                    }
+                })
+                .then(() => {
+                    movieScrapper.getMoviesInfoFromUrls(moviesQueue);
+                });
+            resolve();
+        });
+        return promise;
     },
     generateActors() {
-        let actorsQueue = queuesFactory.getQueue();
-        let actorUrlTemplate = _.template("http://www.imdb.com/name/<%=actorId%>");
+        let promise = new Promise((resolve) => {
+            let actorsQueue = queuesFactory.getQueue();
+            let actorUrlTemplate = _.template("http://www.imdb.com/name/<%=actorId%>");
 
-        modelsFactory.showMovies()
-            .then((movies) => {
-                for (let movie of movies) {
-                    for (let actor of movie.actorIDs) {
-                        let url = actorUrlTemplate({ actorId: actor });
-                        actorsQueue.push(url);
+            modelsFactory.showMovies()
+                .then((movies) => {
+                    for (let movie of movies) {
+                        for (let actor of movie.actorIDs) {
+                            let url = actorUrlTemplate({ actorId: actor });
+                            actorsQueue.push(url);
+                        }
                     }
-                }
-            })
-            .then(() => {
-                actorScrapper.getActorsFromUrls(actorsQueue);
-            });
+                })
+                .then(() => {
+                    actorScrapper.getActorsFromUrls(actorsQueue);
+                });
+            resolve();
+        });
+        return promise;
     }
 };
